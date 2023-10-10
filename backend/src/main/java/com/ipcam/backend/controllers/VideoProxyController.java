0a055098ace06bbdf385e30c1bed563d28aa1af2
Amanda <amandawangyr@gmail.com>
2023-10-10 15:17:06 +0800

    implementing proxy logic

--- backend/src/main/java/com/ipcam/backend/controllers/VideoProxyController.java
+++ backend/src/main/java/com/ipcam/backend/controllers/VideoProxyController.java
@@ -5,6 +5,7 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -12,16 +13,22 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 import java.io.IOException;
 
 @RestController
 public class VideoProxyController {
 
+    @Value("${wip01.cam.01.ip}")
+    private String wip0101Ip;
+
     @GetMapping("/proxy-video")
     public ResponseEntity<byte[]> proxyVideo() throws IOException {
+        String camURL = "http://" + wip0101Ip + "/mjpg/video.mjpg?&camera=1&overview=1&resolution=1920x1080";
+        System.out.println(camURL);
         // Use HttpClient to fetch the HTTP video content
         CloseableHttpClient httpClient = HttpClients.createDefault();
-        HttpGet httpGet = new HttpGet("http://example.com/original-video-url");
+        HttpGet httpGet = new HttpGet(camURL);
         CloseableHttpResponse response = httpClient.execute(httpGet);
 
         // Read the content and return it as a response
@@ -30,4 +37,5 @@ public class VideoProxyController {
         headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
         return new ResponseEntity<>(videoData, headers, HttpStatus.OK);
     }
+
 }
